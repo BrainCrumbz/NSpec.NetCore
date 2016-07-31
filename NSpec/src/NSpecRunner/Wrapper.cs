7715a2fa9f3c212409ce5b24030bc26999e97978
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-07-31 14:09:05 +0200

    chore(solution): add original NSpecRunner project source files

---
+++ NSpec/src/NSpecRunner/Wrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NSpec.Domain;
+
+namespace NSpecRunner
+{
+    public class Wrapper : MarshalByRefObject
+    {
+        public int Execute(RunnerInvocation invocation, Func<RunnerInvocation, int> action)
+        {
+            return action(invocation);
+        }
+
+        public override object InitializeLifetimeService()
+        {
+            return null;
+        }
+    }
+}
