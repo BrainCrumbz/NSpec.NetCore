bf5b8bd18f98892d9b5d0b83e5f25b6cbc826a89
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-08-06 10:30:11 +0200

    chore(solution): add original NSpecSpecs project source files

---
+++ NSpec/test/NSpecSpecs/describe_RunningSpecs/Exceptions/when_method_level_after_all_contains_exception.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using NSpec;
+using NSpec.Domain;
+using NSpecSpecs.WhenRunningSpecs;
+using NUnit.Framework;
+
+namespace NSpecSpecs.describe_RunningSpecs.Exceptions
+{
+    [TestFixture]
+    [Category("RunningSpecs")]
+    public class when_method_level_after_all_contains_exception : when_running_specs
+    {
+        class SpecClass : nspec
+        {
+            void after_all()
+            {
+                throw new AfterAllException();
+            }
+
+            void should_fail_this_example()
+            {
+                it["should fail"] = () => "hello".should_be("hello");
+            }
+
+            void should_also_fail_this_example()
+            {
+                it["should also fail"] = () => "hello".should_be("hello");
+            }
+        }
+
+        [SetUp]
+        public void setup()
+        {
+            Run(typeof(SpecClass));
+        }
+
+        [Test]
+        public void the_first_example_should_fail_with_framework_exception()
+        {
+            classContext.AllExamples()
+                        .First()
+                        .Exception
+                        .should_cast_to<ExampleFailureException>();
+        }
+
+        [Test]
+        public void the_second_example_should_fail_with_framework_exception()
+        {
+            classContext.AllExamples()
+                        .Last()
+                        .Exception
+                        .should_cast_to<ExampleFailureException>();
+        }
+
+        class AfterAllException : Exception { }
+    }
+}
---
+++ NSpec/test/NSpecSpecs/describe_RunningSpecs/describe_xcontext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using NSpec;
+using NUnit.Framework;
+
+namespace NSpecSpecs.WhenRunningSpecs
+{
+    [TestFixture]
+    [Category("RunningSpecs")]
+    [Category("Pending")]
+    public class describe_it_behaviour_in_xcontext : when_running_specs
+    {
+        class SpecClass : nspec
+        {
+            void method_level_context()
+            {
+                xcontext["sub context"] = () =>
+                {
+                    it["needs an example or it gets filtered"] =
+                        () => "Hello World".should_be("Hello World");
+                };
+            }
+        }
+
+        [SetUp]
+        public void setup()
+        {
+            Run(typeof(SpecClass));
+        }
+
+        [Test]
+        public void the_example_should_be_pending()
+        {
+            methodContext.Contexts.First().Examples.First().Pending.should_be(true);
+        }
+    }
+
+    [TestFixture]
+    [Category("RunningSpecs")]
+    [Category("Pending")]
+    public class describe_xcontext : when_running_specs
+    {
+        class SpecClass : nspec
+        {
+            public static string output = string.Empty;
+            public static Action MethodLevelBefore = () => { throw new Exception("this should not run."); };
+            public static Action SubContextBefore = () => { throw new Exception("this should not run."); };
+
+            void method_level_context()
+            {
+                before = MethodLevelBefore;
+
+                xcontext["sub context"] = () =>
+                {
+                    before = SubContextBefore;
+
+                    it["needs an example or it gets filtered"] =
+                        () => "Hello World".should_be("Hello World");
+                };
+            }
+        }
+
+        [SetUp]
+        public void setup()
+        {
+            Run(typeof(SpecClass));
+        }
+
+        [Test]
+        public void it_should_not_run_befores_on_pending_context()
+        {
+            methodContext.AllExamples().First().Exception.should_be(null);
+        }
+    }
+}
